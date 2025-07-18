3f1f3665994a479129cc2fdcf3f0e8a8b983d9a3
Annai22 <annai.nunez@alumnos.uv.cl>
2025-07-18 19:10:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,28 @@ const ramos = [
       "Programación",
       "Hardware Digital",
       "Matemáticas Discretas"
+       ]
+   },
+  {
+    nombre: "Desarrollo personal",
+    semestre: 1,
+    abre: [
+      "Inglés básico"
     ]
-  },
+   },
+  {
+    nombre: "Desafíos de ingeniería",
+    semestre: 1,
+    abre: [
+      "Ingeniería innovación y emprendimiento"
+    ]
+   },
+  {
+    nombre: "Química para ingeniería",
+    semestre: 1,
+    abre: [
+    ]
+   },
   {
     nombre: "Cálculo Diferencial",
     semestre: 2,
@@ -37,22 +57,476 @@ const ramos = [
     ]
   },
   {
-    nombre: "Programación Avanzada",
+    nombre: "Fundamentos de Física",
+    semestre: 2,
+    abre: [
+      "Física mecánica"
+    ]
+   },
+  {
+    nombre: "Álgebra",
+    semestre: 2,
+    abre: [
+      "Álgebra lineal"
+    ]
+   },
+  {
+    nombre: "Ingeniería innovación y emprendimiento",
+    semestre: 2,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Algebra líneal",
     semestre: 3,
     abre: [
-      "Estructuras de Datos",
-      "Modelos de Base de Datos",
-      "Fundamentos de Ingeniería de Software"
+      "Ecuaciones diferenciales lineales"
     ]
-  },
+   },
   {
-    nombre: "Sistemas Operativos",
-    semestre: 6,
+    nombre: "Cálculo integral y series",
+    semestre: 3,
     abre: [
-      "Pruebas de Sistemas",
+      "Cálculos en varias variables"
+      "Ecuaciones diferenciales ordinarias"
+      "Física calor y ondas"
+      "Física electromagnetismo"
+    ]
+   },
+  {
+    nombre: "Física mecánica",
+    semestre: 3,
+    abre: [
+      "Física calor y ondas"
+      "Física electromagnetismo"
+    ]
+   },
+  {
+    nombre: "Programación avanzada",
+    semestre: 3,
+    abre: [
+      "Estructura de datos"
+      "Modelos de bases de datos"
+      "Fundamentos de ingeniería de software"
+      "Laboratorio de redes de computadores"
+      "Aplicaciones web"
+      "Simulación"
+    ]
+   },
+  {
+    nombre: "Hardware digital",
+    semestre: 3,
+    abre: [
+      "Redes de computadores"
+      "Estructura de datos"
+      "Arquitectura de computadores"
+    ]
+   },
+  {
+    nombre: "Inglés básico",
+    semestre: 3,
+    abre: [
+      "Inglés elemental"
+    ]
+   },
+  {
+    nombre: "Cálculo en varias variables",
+    semestre: 4,
+    abre: [
+      "Práctica profesional 1"
+      "Taller de integración del perfil sello uv 1"
+      "Estadística computacional"
+      "Ética"
+    ]
+   },
+  {
+    nombre: "Ecuciones diferenciales ordinarias",
+    semestre: 4,
+    abre: [
+      "Práctica profesional 1"
+      "Taller de integración del perfil sello uv"
+      "Ética"
+    ]
+   },
+  {
+    nombre: "Física calor y ondas",
+    semestre: 4,
+    abre: [
+      "Ética"
+      "Taller de integracion del perfil sello uv"
+      "Práctica profesional 1"
+    ]
+   },
+  {
+    nombre: "Redes de computadores",
+    semestre: 4,
+    abre: [
+      "Ética"
+      "Taller de integración del perfil sello uv"
+      "Práctica profesional 1"
+    ]
+   },
+  {
+    nombre: "Matemáticas discretas",
+    semestre: 4,
+    abre: [
+      "Ética"
+      "Taller de integración del perfil sello uv"
+      "Práctica profesional 1"
+    ]
+   },
+  {
+    nombre: "Inglés elemental",
+    semestre: 4,
+    abre: [
+      "Práctica profesional 2"
+    ]
+   },
+  {
+    nombre: "Práctica profesional 1",
+    semestre: 4,
+    abre: [
+      "Práctica profesional 2"
+    ]
+   },
+  {
+    nombre: "Física electromagnetismo",
+    semestre: 5,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Estructura de datos",
+    semestre: 5,
+    abre: [
+      "Análisis y diseño de algoritmos"
+      "Lenguajes autómatas"
+    ]
+   },
+  {
+    nombre: "Arquitectura de computadores",
+    semestre: 5,
+    abre: [
+      "Aplicaciones web"
+      "Experiencia del usuario"
+    ]
+   },
+  {
+    nombre: "Modelos de base de datos",
+    semestre: 5,
+    abre: [
+      "Aplicaciones web"
+      "Experiencia del usuario"
+    ]
+   },
+  {
+    nombre: "Fundamentos de ingeniería de software",
+    semestre: 5,
+    abre: [
+      "Pruebas de sistemas"
+      "Ingeniería de requisitos"
+      "Sistemas de información"
       "Ciberseguridad"
     ]
-  }
+   },
+  {
+    nombre: "Laboratorio de redes de computadores",
+    semestre: 5,
+    abre: [
+      "Redes avanzadas de computadores"
+    ]
+   },
+  {
+    nombre: "Inglés intermedio",
+    semestre: 5,
+    abre: [
+      "Inglés avanzado"
+    ]
+   },
+  {
+    nombre: "Análisis y diseño de algoritmos",
+    semestre: 6,
+    abre: [
+      "Estádistica computacional"
+      "Taller de integración tecnológica"
+      "Administración general"
+      "Fundamentos de inteligencia artificial"
+      "Electivo profesional 1"
+    ]
+   },
+  {
+    nombre: "Sistemas operativos",
+    semestre: 6,
+    abre: [
+      "Pruebas de sistemas"
+      "Redes  avanzadas de computadores"
+      "Taller de ingtegración tecnológica"
+      "Administración general"
+      "Ciberseguridad", "Electivo profesional 1"
+    ]
+   },
+  {
+    nombre: "Aplicaciones web",
+    semestre: 6,
+    abre: [
+      "Administración de base de datos"
+      "Prueba de sistemas"
+      "Taller de integracion tecnológica"
+      "Administración general"
+      "Ciberseguridad", "Electivo profesional 1"
+    ]
+   },
+  {
+    nombre: "Ingeniería de requisitos",
+    semestre: 6,
+    abre: [
+      "Arquitectura de software"
+      "Taller de integracion tecnológica"
+      "Administración general"
+      "Electivo profesional 1"
+    ]
+   },
+  {
+    nombre: "Experiencia del usuario",
+    semestre: 6,
+    abre: [
+      "Taller de integración tecnológica"
+      "Administración general"
+      "Electivo profesional 1"
+    ]
+   },
+  {
+    nombre: "Inglés avanzado",
+    semestre: 6,
+    abre: [
+      "Taller de integrtación tecnológica"
+      "Administración general"
+      "Electivo profesional 1"
+    ]
+   },
+  {
+    nombre: "Taller de integración del perfil sello uv 1",
+    semestre: 6,
+    abre: [
+      "Taller de integración tecnológica"
+      "Administración general"
+      "Electivo profesional 1"
+      "Taller de integracion del perfil sello uv 2"
+    ]
+   },
+  {
+    nombre: "Estadística computacional",
+    semestre: 7,
+    abre: [
+      "Fundamentos de inteligencia artificial"
+      "Simulación"
+      "Electivo profesional 2"
+      "Electivo profesional 3"
+    ]
+   },
+  {
+    nombre: "Lenguajes autómatas",
+    semestre: 7,
+    abre: [
+      "Lenguajes de programación"
+      "Electivo profesional 2"
+      "Electivo profesional 3"
+    ]
+   },
+  {
+    nombre: "Arquitectura de software",
+    semestre: 7,
+    abre: [
+      "Gestión de proyectos informáticos"
+      "Electivo profesional 2"
+      "Electivo profesional 3"
+    ]
+   },
+  {
+    nombre: "Sistemas de información",
+    semestre: 7,
+    abre: [
+      "Electivo profesional 2"
+      "Electivo profesional 3"
+    ]
+   },
+  {
+    nombre: "Administración de base de datos",
+    semestre: 7,
+    abre: [
+      "Ciencia de datos"
+      "Electivo profesional 2"
+      "Electivo profesional 3"
+    ]
+   },
+  {
+    nombre: "Ética",
+    semestre: 7,
+    abre: [
+      "Contabilidad y finanzas"
+      "Electivo profesional 2"
+      "Electivo profesional 3"
+    ]
+   },
+  {
+    nombre: "Taller de integración del perfil sello uv 2",
+    semestre: 7,
+    abre: [
+      "Electivo profesional 2"
+      "Electivo profesional 3"
+    ]
+   },
+  {
+    nombre: "Pruebas de sistemas",
+    semestre: 8,
+    abre: [
+      "Práctica profesional"
+      "Proyecyo de título"
+      "Elctivo profesional"
+    ]
+   },
+  {
+    nombre: "Redes avanzadas de computadores",
+    semestre: 8,
+    abre: [
+      "Práctica profesional 2"
+      "Proyecto de título"
+      "Electivo profesional 4"
+    ]
+   },
+  {
+    nombre: "Lenguajes de programación",
+    semestre: 8,
+    abre: [
+      "Práctica profesional 2"
+      "Proyecto de título"
+      "Electivo profesional 4"
+    ]
+   },
+  {
+    nombre: "Ciencia de datos",
+    semestre: 8,
+    abre: [
+      "Práctica profesional 2"
+      "Inteligencia de negocios"
+      "Proyecto de título"
+      "Electivo profesional 4"
+    ]
+   },
+  {
+    nombre: "Taller de integración tecnológica",
+    semestre: 8,
+    abre: [
+      "Práctica profesional"
+      "Proyecto de título"
+      "Electivo profesional 4"
+    ]
+   },
+  {
+    nombre: "Administración general",
+    semestre: 8,
+    abre: [
+      "Práctica profesional 2"
+      "Economía"
+      "Evaluación de proyectos"
+      "Proyecto de título"
+      "Electivo profesional 4"
+    ]
+   },
+  {
+    nombre: "Contabilidad y finanzas",
+    semestre: 8,
+    abre: [
+      "Práctica profesional 2"
+      "Proyecto de título"
+      "Evaluación de proyectos"
+      "Electivo profesional 4"
+    ]
+   },
+  {
+    nombre: "Práctica profesional 2",
+    semestre: 8,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Gestión de proyectos informáticos",
+    semestre: 9,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Fundamentos de inteligencia artificial",
+    semestre: 9,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Simulación",
+    semestre: 9,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Ciberseguridad",
+    semestre: 9,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Economía",
+    semestre: 9,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Electivo profesional 1",
+    semestre: 9,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Evaluación de proyectos",
+    semestre: 10,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Inteligencia de negocios",
+    semestre: 10,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Proyecto de título",
+    semestre: 10,
+    abre: [
+      "Seminario de título"
+    ]
+   },
+  {
+    nombre: "Electivo profesional 2",
+    semestre: 10,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Electivo profesional 3",
+    semestre: 10,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Seminario de título",
+    semestre: 11,
+    abre: [
+    ]
+   },
+  {
+    nombre: "Electivo profesional 4",
+    semestre: 11,
+    abre: [
+    
   // Puedes seguir agregando más ramos aquí...
 ];
 
@@ -71,3 +545,5 @@ ramos.forEach(ramo => {
   };
   contenedor.appendChild(div);
 });
+
+
